da0b81d89dd537b3e9695bf0dd0e5efadf496e66
Ketan Dudka <ketandudka566123@gmail.com>
2025-08-12 12:34:34 +0530

    push

--- src/comp/Main_panel/MainPanel.jsx
+++ src/comp/Main_panel/MainPanel.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MianPanel.scss";
 import Sidebar from "../sidebar/Sidebar";
 const MainPanel = ({ children }) => {
+    const [active,setActive] = useState(false)
   return (
     <>
       <div class="mainpanel_parent parent">
         <div class="mainpanel">
-          <div class="sidebar_section">
-            <Sidebar />
+          <div class={active ? "sidebar_section active" : "sidebar_section"} onClick={()=>setActive(!active)} >
+            <Sidebar active={active} />
           </div>
-          <div class="main_panel_dection">{children}</div>
+          <div class={active ? "main_panel_dection active" : "main_panel_dection"}>{children}</div>
         </div>
       </div>
     </>
--- src/comp/sidebar/Sidebar.jsx
+++ src/comp/sidebar/Sidebar.jsx
@@ -1,16 +1,59 @@
-import React from 'react'
+import React from "react";
 import "./Sidebar.scss";
-
-const Sidebar = () => {
+import logo from "../../assets/logo-2.webp";
+import small_logo from "../../assets/small_logo.webp";
+import { Link } from "react-router-dom";
+import { IoGrid } from "react-icons/io5";
+import { FaPeopleLine } from "react-icons/fa6";
+const Sidebar = ({ active }) => {
+  const navlinks = [
+    {
+      name: "Dashboard",
+      path: "/",
+      icon: <IoGrid />,
+    },
+    {
+      name: "Clients",
+      path: "/clients",
+      icon: <FaPeopleLine />,
+    },
+  ];
   return (
     <>
       <div class="sidebar">
-        <div class="sidebar_section">
-
+        <div class="sidebar_section_inner">
+          <div class="logo">
+            {active ? (
+              <img src={small_logo} alt="" />
+            ) : (
+              <img src={logo} alt="" />
+            )}
+          </div>
+          {!active ? (
+            <div class="sidebar_items">
+              {navlinks &&
+                navlinks.map((item, index) => (
+                  <Link key={index} to={item.path}>
+                    {" "}
+                    {item.name}{" "}
+                  </Link>
+                ))}
+            </div>
+          ) : (
+            <div class="toggle_sidbar_icons">
+              {navlinks &&
+                navlinks.map((item, index) => (
+                  <Link key={index} to={item.path}>
+                    {" "}
+                    {item.icon}{" "}
+                  </Link>
+                ))}
+            </div>
+          )}
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
