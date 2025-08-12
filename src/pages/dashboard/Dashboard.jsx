1984812ccdf1de7d26a3b8be7d75aa83e6211661
candyjo-72 <78309347+candyjo-72@users.noreply.github.com>
2025-08-12 11:47:21 +0530

    table

---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Table from "../../comp/table/Table";
+
+const Dashboard = () => {
+  const columns = [
+    { title: "User", dataIndex: "user", key: "user" },
+    { title: "Login time", dataIndex: "loginTime", key: "loginTime" },
+    { title: "Login date", dataIndex: "loginDate", key: "loginDate" },
+    { title: "Logout time", dataIndex: "logoutTime", key: "logoutTime" },
+    
+  ];
+
+  const data = [
+    {
+      key: "1",
+      user: "ashok_dhas",
+      loginTime: "10:28 am",
+      loginDate: "11 Aug 2025",
+      logoutTime: "still logged in",
+    },
+    {
+      key: "2",
+      user: "ashok_dhas",
+      loginTime: "05:33 pm",
+      loginDate: "8 Aug 2025",
+      logoutTime: "still logged in",
+    },
+  ];
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Dashboard</h2>
+      <Table data={data} columns={columns} />
+    </div>
+  );
+};
+
+export default Dashboard;
--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -1,6 +1,7 @@
 import { lazy } from "react";
 
 const Home = lazy(() => import("../pages/Home/Home.jsx"));
+const Dashboard = lazy(() => import("../pages/dashboard/Dashboard.jsx"));
 
 export const routes = [
   {
@@ -8,5 +9,11 @@ export const routes = [
     exact: true,
     name: "home",
     component: Home,
+  },
+  {
+    path: "/dashboard",
+    exact: true,
+    name: "dashboard",
+    component: Dashboard,
   },
 ];
