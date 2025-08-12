09fc66e1a992ff4e55f3df48f160e5c375edb8ec
Ketan Dudka <ketandudka566123@gmail.com>
2025-08-12 18:04:51 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import "./App.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { routes } from "./routes/Route";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <>
       <div class="app">
         <BrowserRouter>
+          <ToastContainer />
           <Routes>
             {routes.map((item, index) => (
               <Route
