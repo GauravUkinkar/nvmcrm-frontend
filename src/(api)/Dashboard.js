40cce4643644d77b9722b68c36ad74ea5e8289d6
candyjo-72 <78309347+candyjo-72@users.noreply.github.com>
2025-08-13 17:43:39 +0530

    employee

---
+++ src/(api)/Dashboard.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+// Add request interceptor to include auth token
+axiosInstance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  });
+
+
+export const loginLogsGetAll = async () => {
+  try {
+    const response = await axiosInstance.get('/admin/getAllLoginLogs');   
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || error;
+  }
+};
