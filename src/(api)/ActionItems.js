586d5f68043736e55b011d59f796b60994c37ff1
candyjo-72 <78309347+candyjo-72@users.noreply.github.com>
2025-08-14 12:09:59 +0530

    actionItems

---
+++ src/(api)/ActionItems.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+// Add request interceptor to include auth token
+axiosInstance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  });
+
+
+export const actionGetAll = async () => {
+  try {
+    const response = await axiosInstance.get('/actionItems/getAllActionItemss');   
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || error;
+  }
+};
+
+export const updateClient = async (data) => {
+  try {
+    const response = await axiosInstance.put('/actionItems/updateActionItems',data);   
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || error;
+  }
+};
+
